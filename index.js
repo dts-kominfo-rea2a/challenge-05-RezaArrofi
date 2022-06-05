0b870615c88f10db9cfb0221e7a50631d48fc25b
RezaArrofi <arrofireza23@gmail.com>
2022-06-05 14:35:33 +0700

    mantap

--- index.js
+++ index.js
@@ -6,15 +6,35 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (list_utama, cbSort) =>{
+  // kirim list_utama ke fungsi cvSort
+  sortlist = cbSort(list_utama);
+
+  // buat array kosong untuk hasil
+  let data = [];
+  // looping nama di list_utama
+  for(let i=0; i<list_utama.length; i++){
+    // push kedalam array data
+    data.push(' '+(i+1)+'. '+list_utama[i]);
+  }
+
+  // mengembalikan array data
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (param) => {
+  let data = param.sort();
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (param) => {
+  let data = param.sort().reverse();
+  return data;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
